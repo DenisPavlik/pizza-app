bd294fa2a86af28898ebb72dfba594272e5c7647
Denys <denys@Denyss-MacBook-Pro.local>
2023-12-18 14:41:18 +0200

    Added login and regestration to the app

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,23 +1,43 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
-export default function Header () {
+export default function Header() {
+  const session = useSession();
+  console.log(session);
+  const status = session.status;
   return (
     <header className="flex item-center justify-between">
-        <Link className="text-primary font-semibold text-2xl" href="">
+      <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+        <Link className="text-primary font-semibold text-2xl" href={"/"}>
           ST PIZZA
         </Link>
-        <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-          <Link href={""}>Home</Link>
-          <Link href={""}>Menu</Link>
-          <Link href={""}>About</Link>
-          <Link href={""}>Contact</Link>
-          <Link
-            href={""}
+        <Link href={"/"}>Home</Link>
+        <Link href={""}>Menu</Link>
+        <Link href={""}>About</Link>
+        <Link href={""}>Contact</Link>
+      </nav>
+      <nav className="flex items-center gap-4 text-gray-500 font-semibold">
+        {status === "authenticated" && (
+          <button
+            onClick={() => signOut()}
             className="bg-primary rounded-full text-white px-8 py-2"
           >
-            Login
-          </Link>
-        </nav>
-      </header>
-  )
+            Logout
+          </button>
+        )}
+        {status === "unauthenticated" && (
+          <>
+            <Link href={"/login"}>Login</Link>
+            <Link
+              href={"/register"}
+              className="bg-primary rounded-full text-white px-8 py-2"
+            >
+              Register
+            </Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
 }
--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -3,7 +3,7 @@ import Right from "../icons/Right";
 
 export default function Hero() {
   return (
-    <section className="hero">
+    <section className="hero mt-4">
       <div className="py-12">
         <h1 className="text-4xl font-semibold">
           Everything
@@ -21,14 +21,14 @@ export default function Hero() {
             Order now
             <Right />
           </button>
-          <button className="flex items-center gap-2 py-2 text-gray-600 font-semibold">
+          <button className="flex items-center border-0 gap-2 py-2 text-gray-600 font-semibold">
             Learn more
             <Right />
           </button>
         </div>
       </div>
       <div className="relative">
-        <Image src={"/pizza.png"} layout={"fill"} objectFit={"contain"} />
+        <Image src={"/pizza.png"} alt="pizza" layout={"fill"} objectFit={"contain"} />
       </div>
     </section>
   );
